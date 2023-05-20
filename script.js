4e456090a4b87c988584e913961ec6611fa256dd
Basit <basitabdullah1122@gmail.com>
2023-05-20 20:46:06 +0530

    Added responsiveness

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 let arrows = document.querySelectorAll('.arrow')
 let movieLists = document.querySelectorAll(".movieList")
 arrows.forEach((arrow,i)=>{
+  
     const itemNumber = movieLists[i].querySelectorAll("img").length
     let clickCounter = 0;
 
   arrow.addEventListener("click",()=>{
+    const ratio =  Math.floor(window.innerWidth/270)
+    console.log(ratio);
     clickCounter++
-    if(itemNumber -(4+clickCounter) >= 0){
+    if(itemNumber -(4 + clickCounter) + (4 - ratio) >= 0){
         movieLists[i].style.transform = `translateX(${movieLists[i].computedStyleMap().get("transform")[0].x.value -300}px)`
 
     }else{
